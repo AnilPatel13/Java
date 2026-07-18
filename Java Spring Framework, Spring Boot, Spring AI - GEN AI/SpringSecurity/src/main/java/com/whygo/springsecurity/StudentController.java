843fcedb755915ce8dce2191dd9b607c901f7fce
Patel, Anilkumar <anilkumar.m.patel@lowes.com>
2026-07-18 18:55:36 -0400

    adding ecommerce project.

---
+++ Java Spring Framework, Spring Boot, Spring AI - GEN AI/SpringSecurity/src/main/java/com/whygo/springsecurity/StudentController.java
@@ -0,0 +1,44 @@
+package com.whygo.springsecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(
+            List.of(
+                    new Student(1, "John", "Spring"),
+                    new Student(2, "Jane", "Spring Boot"),
+                    new Student(3, "Jack", "Spring Security"),
+                    new Student(4, "Jill", "Spring Cloud"),
+                    new Student(5, "XXXX", "Spring Cloud"),
+                    new Student(6, "YYYY", "Spring Cloud")
+
+            )
+    );
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("/students")
+    public void addStudent(@RequestBody Student student) {
+        students.add(student);
+    }
+
+
+}
